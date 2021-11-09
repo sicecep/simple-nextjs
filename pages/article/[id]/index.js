f95958b8b238b211b7ba217d36979b7dd3dba8e1
Sepry Haryandi <sepry.haryandi@tiptip.tv>
2021-11-09 14:59:24 +0700

    update config server

--- pages/article/[id]/index.js
+++ pages/article/[id]/index.js
@@ -1,7 +1,6 @@
-import { server } from '../../../config'
 import Link from 'next/link'
-import { useRouter } from 'next/router'
 import Meta from '../../../components/Meta'
+import { server } from '../../../config'
 
 const article = ({ article }) => {
   // const router = useRouter()
