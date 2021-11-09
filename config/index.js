202a71f365c6bd8cf5ca745cc4bc8518baed4d44
Sepry Haryandi <sepry.haryandi@tiptip.tv>
2021-11-09 10:02:10 +0700

    add deploy prod url

--- config/index.js
+++ config/index.js
@@ -1,5 +1,5 @@
 const dev = process.env.NODE_ENV !== 'production'
 
-export const server = dev ? 'http://localhost:3000' : 'http://localhost:3000';
+export const server = dev ? 'http://localhost:3000' : 'https://live-stream-tip2.netlify.app';
 // https://maxbase.org
 
